f9c025a70a09dbd46f4ba0fef2364229bf288324
Andrew Pau <apau4180@berkeley.edu>
2017-09-14 01:10:44 -0700

    Binary Tree traversal by level

---
+++ binaryTreeTraversal.py
@@ -0,0 +1,29 @@
+from collections import deque
+"""
+Prints all values in the binary tree level by level, from left to right.
+"""
+def solution(node):
+    queue = deque([])
+    queue.append(node)
+    while len(queue) > 0:
+        node = queue.popleft()
+        if node == None:
+            continue
+        print(node.val)
+        queue.append(node.left)
+        queue.append(node.right)
+
+
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+root = Node(4)
+root.right = Node(3)
+root.right.right = Node(-1)
+root.right.right.left = Node(-2)
+root.left = Node(21)
+root.left.right = Node(41)
+solution(root)
